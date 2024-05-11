9cf90017775d21185767799aca2588ca4b1ac89b
Evan Wise <evan.d.wise@gmail.com>
2024-05-11 13:08:58 -0500

    Add windows specific section to wezterm config.

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,13 +1,13 @@
 -- Author: Evan Wise
--- Revision Date: 2024-05-05
+-- Revision Date: 2024-05-11
 -- Purpose: Configuration for wezterm terminal emulator
 
-local wezterm = require 'wezterm';
+local wezterm = require('wezterm');
 
 local config = wezterm.config_builder();
 
 config.font = wezterm.font("Fira Code");
-config.font_size = 12.0;
+config.font_size = 14.0;
 config.color_scheme = "Selenized Dark (Gogh)";
 config.hide_tab_bar_if_only_one_tab = true;
 config.window_padding = {
@@ -19,5 +19,12 @@ config.window_padding = {
 config.keys = {
   {key="x", mods="CTRL", action=wezterm.action{CloseCurrentTab={confirm=true}}},
 };
+
+-- Windows specific overrides
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.color_scheme = "Tokyo Night Storm";
+  config.default_prog = {"powershell.exe", "-NoLogo"};
+end
+
 
 return config;
