d545d0b78c364d337cbbdeb67265d0108ff9cb02
Evan Wise <evan.d.wise@gmail.com>
2024-08-10 18:52:29 -0500

    feat(main): add neotest test runner

--- home/.config/nvim/lua/plugins/experimental.lua
+++ home/.config/nvim/lua/plugins/experimental.lua
@@ -20,4 +20,34 @@ return {
       vim.keymap.set('n', '<leader>ft', ':NvimTreeToggle<CR>', opts)
     end,
   },
+  {
+    'nvim-neotest/neotest',
+    enabled = true, -- Change to false to disable
+    version = '*',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-python',
+      'nvim-neotest/neotest-jest',
+      'marilari88/neotest-vitest',
+    },
+    config = function()
+      local neotest = require('neotest')
+      -- This line works even though lazydev.nvim complains.
+      neotest.setup({
+        adapters = {
+          require('neotest-python')({}),
+          require('neotest-jest')({}),
+          require('neotest-vitest')({}),
+        },
+      })
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '<leader>tr', ':Neotest run<CR>', opts)
+      vim.keymap.set('n', '<leader>ts', ':Neotest stop<CR>', opts)
+      vim.keymap.set('n', '<leader>ta', ':Neotest attach<CR>', opts)
+      vim.keymap.set('n', '<leader>tt', ':Neotest summary<CR>', opts)
+    end,
+  },
 }
