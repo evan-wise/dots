3c0ebc3a4d74252a94f5fb3686f801cce7e281dc
Evan Wise <evan.d.wise@gmail.com>
2024-08-13 18:59:11 -0500

    feat(main): add faster fzf for telescope

--- home/.config/nvim/lua/plugins/core.lua
+++ home/.config/nvim/lua/plugins/core.lua
@@ -60,9 +60,21 @@ return {
           find_files = {
             find_command = { 'rg', '--files', '--hidden', '--follow', '--glob', '!.git' },
           },
-        }
+        },
+        extensions = {
+          fzf = {
+            -- fuzzy = true,
+            -- override_generic_sorter = true,
+            -- override_file_sorter = true,
+            -- case_mode = 'smart_case',
+          },
+        },
       })
     end,
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
   },
   {
     'kylechui/nvim-surround',
